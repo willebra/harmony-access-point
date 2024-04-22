be6261b8c1b7a0ae75c2e8a87c5f87f566a73788
François <francois.gauti3r@gmail.com>
2024-04-22 16:12:46 +0200

    [EDELIVERY-13182] Issue with the ongoing messages sanitizer

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/message/retention/OngoingMessageSanitizingService.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/message/retention/OngoingMessageSanitizingService.java
@@ -1,8 +1,11 @@
 package eu.domibus.core.message.retention;
 
+import eu.domibus.api.property.DomibusPropertyProvider;
 import eu.domibus.api.util.DateUtil;
 import eu.domibus.core.earchive.EArchiveBatchUserMessage;
 import eu.domibus.core.message.UserMessageLogDao;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 import static eu.domibus.api.model.DomibusDatePrefixedSequenceIdGeneratorGenerator.MIN;
+import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMIBUS_ONGOING_MESSAGES_SANITIZING_WORKER_DELAY_HOURS;
 
 /**
  * @author Cosmin Baciu
@@ -18,21 +22,30 @@ import static eu.domibus.api.model.DomibusDatePrefixedSequenceIdGeneratorGenerat
 @Service
 public class OngoingMessageSanitizingService {
 
-    private MessageRetentionPartitionsService messageRetentionPartitionsService;
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(OngoingMessagesSanitizingWorker.class);
 
-    private DateUtil dateUtil;
+    private final MessageRetentionPartitionsService messageRetentionPartitionsService;
 
-    private UserMessageLogDao userMessageLogDao;
+    private final DateUtil dateUtil;
 
-    public OngoingMessageSanitizingService(MessageRetentionPartitionsService messageRetentionPartitionsService, DateUtil dateUtil, UserMessageLogDao userMessageLogDao) {
+    private final UserMessageLogDao userMessageLogDao;
+
+    private final DomibusPropertyProvider domibusPropertyProvider;
+
+    public OngoingMessageSanitizingService(MessageRetentionPartitionsService messageRetentionPartitionsService,
+                                           DateUtil dateUtil,
+                                           UserMessageLogDao userMessageLogDao,
+                                           DomibusPropertyProvider domibusPropertyProvider) {
         this.messageRetentionPartitionsService = messageRetentionPartitionsService;
         this.dateUtil = dateUtil;
         this.userMessageLogDao = userMessageLogDao;
+        this.domibusPropertyProvider = domibusPropertyProvider;
     }
 
     public List<EArchiveBatchUserMessage> findOngoingMessagesWhichAreNotProcessedAnymore() {
-        int maxRetention = messageRetentionPartitionsService.getMaxRetention();
         long lastMessageId = Long.MAX_VALUE;
+
+        int maxRetention = getMaxRetention();
         if(maxRetention > 0) {
             Date lastDateNotBeingProcessed = DateUtils.addMinutes(dateUtil.getUtcDate(), maxRetention * -1);
             String idPrefix = dateUtil.getIdPkDateHourPrefix(lastDateNotBeingProcessed);
@@ -41,4 +54,23 @@ public class OngoingMessageSanitizingService {
         List<EArchiveBatchUserMessage> messagesNotFinalAsc = userMessageLogDao.findMessagesNotFinalAsc(0, lastMessageId);
         return messagesNotFinalAsc;
     }
+
+    /**
+     * @return maximum value between the maxRetention and the retry timeout
+     */
+    private int getMaxRetention() {
+        int maxRetention = messageRetentionPartitionsService.getMaxRetention();
+        long retryTimeoutMin = getSanitizerDelayHours() * 60;
+        return Integer.max((int) retryTimeoutMin, maxRetention);
+    }
+
+    protected long getSanitizerDelayHours() {
+        Long delay = domibusPropertyProvider.getLongProperty(DOMIBUS_ONGOING_MESSAGES_SANITIZING_WORKER_DELAY_HOURS);
+        if (delay == null) {
+            LOG.debug("No value found for [{}]. Use no further delay", DOMIBUS_ONGOING_MESSAGES_SANITIZING_WORKER_DELAY_HOURS);
+            return 0L;
+        }
+        return delay;
+    }
+
 }
---
+++ Core/Domibus-MSH/src/test/java/eu/domibus/core/message/retention/OngoingMessageSanitizingServiceIT.java
@@ -0,0 +1,11 @@
+package eu.domibus.core.message.retention;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author François Gautier
+ * @since 5.1
+ */
+public class OngoingMessageSanitizingServiceIT {
+
+}
